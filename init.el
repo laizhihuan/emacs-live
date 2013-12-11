aa21f1fdcc8ed3321aa22078bace5a68d593356b
laizhihuan <laizhihuan@basecity.com>
2013-12-11 11:42:26 +0800

    中文显示问题

--- init.el
+++ init.el
@@ -212,6 +212,13 @@
 ")))
 )
 
+(create-fontset-from-fontset-spec
+"-apple-bitstream vera sans mono-medium-r-normal--12-*-*-*-*-*-fontset-mymonaco,
+chinese-gb2312:-apple-STHeiti-medium-normal-normal-12-*-*-*-*-p-0-iso10646-1")
+
+(setq default-frame-alist (append '((font . "fontset-mymonaco")) default-frame-alist))
+(set-default-font "fontset-mymonaco")
+
 (if (not live-disable-zone)
     (add-hook 'term-setup-hook 'zone))
 
