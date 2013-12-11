833b76fa05de9dab97102b9a7c3f9d13696a4cdc
laizhihuan <laizhihuan@basecity.com>
2013-12-11 12:04:13 +0800

    style

--- init.el
+++ init.el
@@ -212,13 +212,6 @@
 ")))
 )
 
-(create-fontset-from-fontset-spec
-"-apple-bitstream vera sans mono-medium-r-normal--12-*-*-*-*-*-fontset-mymonaco,
-chinese-gb2312:-apple-STHeiti-medium-normal-normal-12-*-*-*-*-p-0-iso10646-1")
-
-(setq default-frame-alist (append '((font . "fontset-mymonaco")) default-frame-alist))
-(set-default-font "fontset-mymonaco")
-
 (if (not live-disable-zone)
     (add-hook 'term-setup-hook 'zone))
 
@@ -226,3 +219,17 @@ chinese-gb2312:-apple-STHeiti-medium-normal-normal-12-*-*-*-*-p-0-iso10646-1")
     (setq custom-file (concat live-custom-dir "custom-configuration.el")))
 (when (file-exists-p custom-file)
   (load custom-file))
+
+;; Load bindings config
+;(live-load-config-file "bindings.el")
+;(live-load-config-file "solarized-conf.el")
+;(live-load-config-file "clean-mode-line.el")
+(set-face-attribute 'vertical-border nil :foreground "red")
+
+(create-fontset-from-fontset-spec
+"-apple-bitstream vera sans mono-medium-r-normal--12-*-*-*-*-*-fontset-mymonaco,
+chinese-gb2312:-apple-STHeiti-medium-normal-normal-12-*-*-*-*-p-0-iso10646-1")
+
+(setq default-frame-alist (append '((font . "fontset-mymonaco")) default-frame-alist))
+(set-default-font "fontset-mymonaco")
+
